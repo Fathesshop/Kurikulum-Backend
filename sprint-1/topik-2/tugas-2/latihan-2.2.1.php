0712e7bd9e3efb03059dbd9114205ab934694640
Fathesshop <59084965+Fathesshop@users.noreply.github.com>
2020-09-08 06:28:00 +0700

    Update latihan-2.2.1.php

--- sprint-1/topik-2/tugas-2/latihan-2.2.1.php
+++ sprint-1/topik-2/tugas-2/latihan-2.2.1.php
@@ -1,6 +1,5 @@
 <?php
 $panjang_persegi = 5;
 $lebar_persegi = 8;
-echo “luas persegi = “.$panjang_persegi*$lebar_persegi;
-echo “keliling persegi = “.(2*panjang_persegi*$lebar_persegi);
+echo "luas persegi = ". $panjang_persegi*$lebar_persegi . "<br>";
 ?>
