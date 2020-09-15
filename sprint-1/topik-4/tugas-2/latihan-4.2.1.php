4acca0fd142798fc6cac506ff1e30ef8157a291f
Fathesshop <Fathesshop@gmail.com>
2020-09-15 14:26:17 +0700

    backend

--- sprint-1/topik-4/tugas-2/latihan-4.2.1.php
+++ sprint-1/topik-4/tugas-2/latihan-4.2.1.php
@@ -37,33 +37,64 @@ $students = [
      'age' => 18,
  ],
 ];
-function data(array $students)
+echo "daftar santri berdasar nomor induk kecil ke besar"."\n";
+function id($bio)
 {
-    foreach ($students as $key => $value)
-    {
-        $id[] = $value["id"];
-        $name[] = $value["name"];
-        $division[] = $value["division"] == ("PHP Backend");
-        $age[] = $value["age"] < 25;
-        $umur_santri[] = $value["age"];
-        $mean_santri = array_sum($umur_santri) / count($umur_santri);
-        $santri_muda = min($umur_santri);
-    }
-    echo "nomor induk kecil ke besar"."\n";
-    array_multisort($id, $students);
-    print_r($id);
-    echo "nama kecil ke besar"."\n";
-    array_multisort($name, $students);
-    print_r($name);
-    echo "minat 'PHP Backend'"."\n";
-    print_r($division);
-    echo "usia kurang dari 25 tahun"."\n";
-    print_r($age);
-    echo "rata-rata usia santri"."\n";
-    print_r($mean_santri);
-    echo "\n";
-    echo "usia paling muda"."\n";
-    print_r($santri_muda);
+    sort($bio);
+    print_r($bio);
 }
-data($students);
-echo "\n";
+id($students);
+echo "daftar santri berdasar nama kecil ke besar"."\n";
+function sortname($bio)
+{
+    foreach ($bio as $key => $value)
+    {
+        $name[] = $value["name"];
+    }
+    array_multisort($name,$bio);
+    print_r($bio);
+}
+sortname($students);
+echo "daftar santri yang minat PHP Backend"."\n";
+function division($bio)
+{
+    foreach ($bio as $key => $value) 
+    {
+        if($bio[$key]["division"]=="PHP Backend")
+        print_r($bio[$key]);
+    } 
+}
+division($students);
+echo "jumlah santri berusia kurang dari 25"."\n";
+function age($bio)
+{
+    foreach ($bio as $key => $value) 
+        {
+            if($bio[$key]["age"]<25)
+            print_r($bio[$key]);
+        } 
+}
+age($students);
+echo "rata-rata usia santri"."\n";
+function mean($bio)
+{
+    foreach ($bio as $key => $value)
+    {
+        $umur_santri[] = $value["age"];
+    }
+    $mean_santri = array_sum($umur_santri) / count($umur_santri);
+    echo $mean_santri."\n";
+}
+mean($students);
+echo "urutan umur santri dari paling muda"."\n";
+function sortage($bio)
+{
+    foreach ($bio as $key => $value)
+    {
+        $age[] = $value["age"];
+    }
+    array_multisort($age,$bio);
+    print_r($bio);
+}
+sortage($students);
+echo "==========TAMAT=========="."\n";
