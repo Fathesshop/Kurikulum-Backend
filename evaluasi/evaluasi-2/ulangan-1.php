3e0bdb5701bedff808021d98e1804d4a87094a12
Fathesshop <Fathesshop@gmail.com>
2020-09-21 14:37:57 +0700

    backend

--- evaluasi/evaluasi-2/ulangan-1.php
+++ evaluasi/evaluasi-2/ulangan-1.php
@@ -4,42 +4,42 @@ class Register
 public static $student =
 [
     [
-        "name" => "Arief",
+        "name" => "Muhammad",
         "nik"  => "TOO1"
     ],
     [
-        "name" => "Ridho",
+        "name" => "Dzulmaarij",
         "nik"  => "TOO2"
-    ],
-    [
-        "name" => "Fauzil",
-        "nik"  => "TOO3"
-    ],
-    [
-        "name" => "Nasrul",
-        "nik"  => "TOO4"
-    ],
-    [
-        "name" => "Rahmat",
-        "nik"  => "TOO5"
     ]
 ];
-
-
     public function __Construct()
     {
     foreach (self::$student as $key => $value) 
         { 
-            echo "Daftar santri baru : " . "\n";
+            echo "========== Data daftar santri baru : ==========" . "\n";
             return print_r(self::$student);
         }
     }
 
-    public function dihapus($hapus)
+    public function update()
     {
-        foreach (self::$student as $key) {
-            return(self::$student[$hapus]);
-    }
+        for ($i=0; $i < count(self::$student); $i++) 
+        { 
+            echo "\n $i => " . "NIK: ".self::$student[$i]['nik'] . " " ."Nama: ".self::$student[$i]['name'];
+        }
+        echo "\nPilih input data yang mau diupdate : ";
+        $update = (int) trim(fgets(STDIN));
+        if ($update >= count(self::$student)) {
+            echo "input data tidak ada \n";
+        }
+        else
+        {
+            echo "ubah nama : ";
+            self::$student[$update]['name'] = trim(fgets(STDIN));;
+            echo "ubah NIK : ";
+            self::$student[$update]['nik'] = trim(fgets(STDIN));;
+            print_r(self::$student);
+        }
     }
 
     public function delete($hapus)
@@ -48,53 +48,53 @@ public static $student =
         return self::$student;
     }
 
-    public function ditambah($tambah)
+    public function add($name, $nik)
     {
-        foreach (self::$student as $key)
-        {
-            return(self::$student[$tambah]);
-        }
-    }
-
-    public function add($tambah)
-    {
-        array_push(self::$student[$tambah]);
-        return self::$student;
+        $nomor = count(self::$student);
+        self::$student[$nomor]['name'] = $name;
+        self::$student[$nomor]['nik'] = $nik;
+        print_r(self::$student);
     }
 }
-
 $Register = new Register ();
+$kondisi = true;
+while ($kondisi) 
+{   echo "====================\n";
+    echo "Pilih Nomor : \n";
+    echo "1. Update Data santri : \n";
+    echo "2. delete data santri : \n";
+    echo "3. tambah data santri : \n";
+    echo "====================\n";
+    $input = trim(fgets(STDIN));
+    switch ($input) 
+    {
+        case 1:
+            print_r ($Register->update());
+        break;
+        
+        case 2:
+            echo "Pilih Nomor input data santri yang mau di delete : ";
+            $hapus = trim(fgets(STDIN));
+            echo "\n========== Sisa data daftar santri baru : ========== \n";
+            print_r ($Register->delete($hapus));
+        break;
 
-echo "Pilih Nomor : " . "\n";
-echo "1. delete santri" . "\n";
-echo "2. tambah santri" . "\n";
-$input = trim(fgets(STDIN));
-switch ($input) 
-{
-    case 1:
-        echo "Pilih Nomor : " . "\n";
-        $hapus = trim(fgets(STDIN));
-
-        echo"Santri yang mau di delete : " . "\n";
-        print_r ($Register->dihapus($hapus));
-
-        echo "Sisa daftar santri baru : " . "\n";
-        print_r ($Register->delete($hapus));
-    break;
-    case 2:
-        echo "Pilih Nomor : " . "\n";
-        $tambah = trim(fgets(STDIN));
-
-        echo"Santri yang mau di dikembalikan : " . "\n";
-        print_r ($Register->ditambah($tambah));
-
-        echo "Sisa daftar santri baru : " . "\n";
-        print_r ($Register->add($tambah));
-
-    break;
-    
-    default:
-        echo "input yang anda masukkan salah" . "\n";
-    break;
+        case 3:
+            echo "Masukkan Nama Santri Baru : ";
+            $name = trim(fgets(STDIN));
+            echo "Masukkan NIK santri baru : ";
+            $nik = trim(fgets(STDIN));
+            echo "\n========== daftar data santri baru terbaru : ==========\n";
+            print_r ($Register->add($name, $nik));
+        break;
+        default:
+            echo "========== input yang anda masukkan salah =========\n";
+        break;
+    }
+        echo "Apakah Anda masih ingin input data? pilih => (y/n)"." = ";
+        $pilih = trim(fgets(STDIN));
+        if($pilih == 'n')
+        {
+            $kondisi = false;
+        }
 }
-echo "BELUM BISA  BUAT PROGRAM KEMBALI KE MENU INPUT MAS SHUFYAN :D" ."\n";
