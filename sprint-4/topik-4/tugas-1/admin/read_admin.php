df0137ac2a33bbc16712ebb14bc2727084d79806
Fathesshop <fathesshop@gmail.com>
2020-10-08 06:23:06 +0700

    backend

--- sprint-4/topik-4/tugas-1/admin/read_admin.php
+++ sprint-4/topik-4/tugas-1/admin/read_admin.php
@@ -5,31 +5,37 @@ if(!$_SESSION["login"]){
     header("location:login.php");
 }
 ?>
-
 <a href="create_admin.php">buat</a>
-<table border="1">
-    <thead>
-        <tr>
-            <th>no.</th>
-            <th>nama</th>
-            <th>username</th>
-            <th>password</th>
-            <th>aksi</th>
-        </tr>
-    <tbody>
-    <?php $query = mysqli_query($con, "SELECT * FROM admin ORDER BY id DESC");
-    foreach ($query as $key => $value) { 
-    ?>
-        <tr>
-            <td><?=$key+1;?></td>
-            <td><?=$value["nama"];?></td>
-            <td><?=$value["username"];?></td>
-            <td><?=$value["password"];?></td>
-            <td>
-                <a href="update_admin.php?id=<?=$value["id"]?>">edit</a> -
-                <a href="delete_admin.php?id=<?=$value["id"]?>">delete</a>
-            </td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
+	<div class="table-responsive">
+		<table class="table table-bordered table-striped table-hover">
+			<thead>
+                <tr>
+                    <th>no.</th>
+                    <th>nama</th>
+                    <th>username</th>
+                    <th>password</th>
+                    <th>aksi</th>
+                </tr>
+			</thead>
+			<tbody>
+                <tr>
+                    <td><?=$key+1;?></td>
+                    <td><?=$value["nama"];?></td>
+                    <td><?=$value["username"];?></td>
+                    <td><?=$value["password"];?></td>
+                    <td>
+                        <a href="update_admin.php?id=<?=$value["id"]?>">edit</a> -
+                        <a href="delete_admin.php?id=<?=$value["id"]?>">delete</a>
+                    </td>
+                </tr>
+			</tbody>
+		</table>
+    </div>
+    
+
+<!--  -->
+
+<?php
+
+include"layout/footer.php";
+?>
