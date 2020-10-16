2d9bc10dd0aa0000e5a157561d1f42a864d2fb4a
Fathesshop <fathesshop@gmail.com>
2020-10-16 12:57:37 +0700

    backend

--- phpmvc/app/config/config.php
+++ phpmvc/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASEURL', 'http://localhost/phpmvc/public');
+define('BASEURL', 'http://localhost/kurikulum-backend/phpmvc/public');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'dzul');
--- phpmvc/public/js/script.js
+++ phpmvc/public/js/script.js
@@ -8,11 +8,11 @@ $(function(){
     $('.tampilModalUbah').on('click', function(){
         $('#formModalLabel').html('Ubah Data Anak Langit');
         $('.modal-footer button[type=submit]').html('Ubah Data');
-        $('.modal-body form').attr('action', 'http://localhost/phpmvc/public/anaklangit/ubah');
+        $('.modal-body form').attr('action', 'http://localhost/kurikulum-backend/phpmvc/public/anaklangit/ubah');
         const id = $(this).data('id');
         console.log(id);
         $.ajax({
-            url : 'http://localhost/phpmvc/public/anaklangit/getubah',
+            url : 'http://localhost/kurikulum-backend/phpmvc/public/anaklangit/getubah',
             data: {id : id},
             method: 'post',
             dataType: 'json',
